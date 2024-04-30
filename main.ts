1a635a766d7ba596ca8a5e75e8a5d9db2317c596
David Quennoz <137565586+david-quennoz-reup@users.noreply.github.com>
2024-04-29 17:06:40 -0700

    got date from JS apis

--- main.ts
+++ main.ts
@@ -1,11 +1,14 @@
 import { parse } from "node-html-parser";
 
 async function getUTCTime() {
-  const response = await fetch("https://time.is/UTC");
-  const html = await response.text();
+  const date = new Date();
+  return date.toUTCString();
 
-  const root = parse(html);
-  return root.querySelector("time")!.textContent;
+  // const response = await fetch("https://time.is/UTC");
+  // const html = await response.text();
+
+  // const root = parse(html);
+  // return root.querySelector("time")!.textContent;
 }
 
 console.log(await getUTCTime());
