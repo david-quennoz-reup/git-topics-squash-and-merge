e4a52059d24a293dc183855b7e9ff8cebd8d04a4
David Quennoz <137565586+david-quennoz-reup@users.noreply.github.com>
2024-05-02 12:23:18 -0700

    Use JS APIs to get UTC time. (#2)
    * got date from JS apis
    
    * closer to the right format
    
    * 24 hour time
    
    * formatting
    
    * remove old code
    
    * remove unused dependencies
    
    * remove async

--- main.ts
+++ main.ts
@@ -1,13 +1,13 @@
-import { parse } from "node-html-parser";
-
-async function getUTCTime() {
-  const response = await fetch("https://time.is/UTC");
-  const html = await response.text();
-
-  const root = parse(html);
-  return root.querySelector("time")!.textContent;
+function getUTCTime() {
+  return new Intl.DateTimeFormat("en-US", {
+    timeZone: "UTC",
+    hour: "numeric",
+    minute: "numeric",
+    second: "numeric",
+    hour12: false,
+  }).format(new Date());
 }
 
-console.log(await getUTCTime());
+console.log(getUTCTime());
 
-export {}
+export {};
